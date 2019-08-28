1133a11e82d1c4a4f23ce076cd0c9b1ab345a389
Matthew Segal <mattdsegal@gmail.com>
2019-08-28 14:33:47 +1000

    Finish script, add deploy

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+HOST=13.211.109.142
+
+echo ">>> Setting up server"
+ssh root@${HOST} /bin/bash << EOF
+    echo ">>> Creating deploy dir"
+    mkdir -p /srv/slackbot
+    if [ ! -d /srv/slackbot/env ]
+    then
+        echo ">>> Creating virtualenv"
+        virtualenv -p python3 /srv/slackbot/env
+    fi
+EOF
+echo ">>> Copying files"
+scp ./requirements.txt root@${HOST}:/srv/slackbot/
+scp ./run.py root@${HOST}:/srv/slackbot/
+scp ./secrets.py root@${HOST}:/srv/slackbot/
+echo ">>> Installing requirements"
+ssh root@${HOST} /bin/bash << EOF
+    . /srv/slackbot/env/bin/activate
+    pip3 install -r /srv/slackbot/requirements.txt
+    echo "DONE"
+EOF
+echo "DOUBLE DONE"
--- run.py
+++ run.py
@@ -1,1 +1,31 @@
-WEBHOOK_URL = 'https://hooks.slack.com/services/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'
+import requests
+import logging
+
+import secrets
+
+logging.basicConfig(
+    filename="slackbot.log",
+    filemode="a",
+    format="[%(asctime)s] %(message)s",
+    datefmt="%d-%m-%Y %H:%M:%S",
+    level=logging.DEBUG,
+)
+
+PROD_WEBHOOK_URL = secrets.PROD_WEBHOOK_URL
+TEST_WEBHOOK_URL = secrets.TEST_WEBHOOK_URL
+
+WEBHOOK_URL = TEST_WEBHOOK_URL
+HEADERS = {"Content-Type": "application/json"}
+TEXT = """
+Hi <!channel>!
+
+It's time for your Weekly Wrap status update. If you did anything Anika related this week, then you should share it with the rest of the team.
+This is important to keep everybody in the loop about what's going on and boost morale.
+
+Please send a message to Kathryn Quinn detailing any milestones, or updates that you have to share.
+
+:heart: Bender :robot_face:
+"""
+
+logging.info("Posting weekly wrap message")
+resp = requests.post(WEBHOOK_URL, json={"text": TEXT}, headers=HEADERS)
